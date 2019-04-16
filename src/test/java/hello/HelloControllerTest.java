59c6fb852759fc23a5c6e961d0e5f60f24ce5a42
Sylvain <sylvain.maucourt@edelia.fr>
2019-04-16 15:39:50 +0200

    test poxiproxy

--- src/test/java/hello/HelloControllerTest.java
+++ src/test/java/hello/HelloControllerTest.java
@@ -23,7 +23,7 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class HelloControllerIT {
+public class HelloControllerTest {
 
     @LocalServerPort
     private int port;
